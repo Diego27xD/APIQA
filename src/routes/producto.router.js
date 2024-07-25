b1600c263ca96bdb71a4983e09fdb368928afc4a
Diego Flores <floresdiego2001xd@gmail.com>
2024-07-25 01:01:50 -0500

    tercer commit

--- src/routes/producto.router.js
+++ src/routes/producto.router.js
@@ -5,11 +5,17 @@ const {
   updateProduct,
   deleteProduct,
 } = require("../controllers/producto.controller");
+const authenticateToken = require("../middleware/auth.verify");
 const router = Router();
 
 router.get("/api/v1/product", getProducts);
 router.post("/api/v1/product", createProduct);
 router.put("/api/v1/product/:IdProducto", updateProduct);
 router.delete("/api/v1/product/:IdProducto", deleteProduct);
+
+router.get("/api/v2/product", authenticateToken, getProducts);
+router.post("/api/v2/product", authenticateToken, createProduct);
+router.put("/api/v2/product/:IdProducto", authenticateToken, updateProduct);
+router.delete("/api/v2/product/:IdProducto", authenticateToken, deleteProduct);
 
 module.exports = { router };
